eaae6cfc07eaa62cfb43ddf9a597c0f0b818396f
suuungwoo <suuungwoo@gmail.com>
2019-06-10 17:12:31 +0900

    追加機能

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   IconButton, Typography, TextField, List, ListItem, ListItemText,
 } from '@material-ui/core';
 import './App.scss';
 
 function App() {
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState([]);
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const todo = {
+      text,
+      done: false,
+    };
+    setTodos([...todos, todo]);
+    document.getElementById('text').value = '';
+  };
   return (
     <div className="App">
       <Typography component="h1" variant="h2">
@@ -16,16 +30,24 @@ function App() {
         <IconButton>完了</IconButton>
       </div>
       <div>
-        <form>
-          <TextField variant="outlined" placeholder="タスクを追加する" />
+        <form onSubmit={handleSubmit}>
+          <TextField
+            id="text"
+            variant="outlined"
+            placeholder="タスクを追加する"
+            onChange={handleChange}
+          />
         </form>
       </div>
       <div>
-        <List>
-          <ListItem button>
-            <ListItemText primary="sample" />
-          </ListItem>
-        </List>
+        {todos
+          && todos.map(todo => (
+            <List>
+              <ListItem button>
+                <ListItemText primary={todo.text} />
+              </ListItem>
+            </List>
+          ))}
       </div>
     </div>
   );
