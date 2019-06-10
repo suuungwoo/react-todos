d403c0a30c2dcfbb1af51c3ef90087a9206bd4be
suuungwoo <suuungwoo@gmail.com>
2019-06-10 18:09:52 +0900

    完了状態切替機能

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 import {
-  IconButton, Typography, TextField, List, ListItem, ListItemText,
+  IconButton,
+  Typography,
+  TextField,
+  List,
+  ListItem,
+  ListItemText,
+  Checkbox,
 } from '@material-ui/core';
 import './App.scss';
 
@@ -12,12 +18,23 @@ function App() {
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    const todo = {
-      text,
-      done: false,
-    };
-    setTodos([...todos, todo]);
-    document.getElementById('text').value = '';
+    const trimmedText = text.trim();
+    if (trimmedText.length > 0) {
+      const getUniqueId = () => `${new Date().getTime().toString(36)}-${Math.random().toString(36)}`;
+      const todo = {
+        id: getUniqueId(),
+        text: trimmedText,
+        done: false,
+      };
+      setTodos([...todos, todo]);
+      document.getElementById('text').value = '';
+      setText('');
+    }
+  };
+  const handleDone = id => () => {
+    const index = todos.findIndex(todo => todo.id === id);
+    todos[index].done = !todos[index].done;
+    setTodos([...todos]);
   };
   return (
     <div className="App">
@@ -41,10 +58,18 @@ function App() {
       </div>
       <div>
         {todos
-          && todos.map(todo => (
+          && todos.map((todo, i) => (
             <List>
-              <ListItem button>
-                <ListItemText primary={todo.text} />
+              <ListItem key={i.toString()} button onClick={handleDone(todo.id)}>
+                <Checkbox
+                  tabIndex={-1}
+                  disableRipple
+                  checked={todo.done}
+                  onChange={() => {
+                    handleDone(todo.id);
+                  }}
+                />
+                <ListItemText primary={todo.text} secondary={todo.done ? '完了' : '未完了'} />
               </ListItem>
             </List>
           ))}
