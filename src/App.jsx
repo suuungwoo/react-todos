46dab05c438d30e3e14e2209934bc3d3993eaf37
suuungwoo <suuungwoo@gmail.com>
2019-06-10 18:31:26 +0900

    フィルタリング機能

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import './App.scss';
 function App() {
   const [text, setText] = useState('');
   const [todos, setTodos] = useState([]);
+  const [filter, setFilter] = useState('all');
   const handleChange = (e) => {
     setText(e.target.value);
   };
@@ -41,11 +42,7 @@ function App() {
       <Typography component="h1" variant="h2">
         React Todos
       </Typography>
-      <div>
-        <IconButton>全て</IconButton>
-        <IconButton>未完了</IconButton>
-        <IconButton>完了</IconButton>
-      </div>
+
       <div>
         <form onSubmit={handleSubmit}>
           <TextField
@@ -57,22 +54,39 @@ function App() {
         </form>
       </div>
       <div>
+        <IconButton onClick={() => setFilter('all')} disabled={filter === 'all'}>
+          全て
+        </IconButton>
+        <IconButton onClick={() => setFilter('done')} disabled={filter === 'done'}>
+          未完了
+        </IconButton>
+        <IconButton onClick={() => setFilter('notDone')} disabled={filter === 'notDone'}>
+          完了
+        </IconButton>
+      </div>
+      <div>
         {todos
-          && todos.map((todo, i) => (
-            <List>
-              <ListItem key={i.toString()} button onClick={handleDone(todo.id)}>
-                <Checkbox
-                  tabIndex={-1}
-                  disableRipple
-                  checked={todo.done}
-                  onChange={() => {
-                    handleDone(todo.id);
-                  }}
-                />
-                <ListItemText primary={todo.text} secondary={todo.done ? '完了' : '未完了'} />
-              </ListItem>
-            </List>
-          ))}
+          && todos
+            .filter(
+              todo => (filter === 'all' && todo)
+                || (filter === 'done' && todo.done)
+                || (filter === 'notDone' && !todo.done),
+            )
+            .map((todo, i) => (
+              <List>
+                <ListItem key={i.toString()} button onClick={handleDone(todo.id)}>
+                  <Checkbox
+                    tabIndex={-1}
+                    disableRipple
+                    checked={todo.done}
+                    onChange={() => {
+                      handleDone(todo.id);
+                    }}
+                  />
+                  <ListItemText primary={todo.text} secondary={todo.done ? '完了' : '未完了'} />
+                </ListItem>
+              </List>
+            ))}
       </div>
     </div>
   );
