32db76621225ff95a60ecf60921fa1cfe61add9b
suuungwoo <suuungwoo@gmail.com>
2019-06-11 21:57:10 +0900

    一括削除機能，スタイリング

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import {
   ListItemText,
   Checkbox,
   ListItemSecondaryAction,
+  Button,
+  ButtonGroup,
 } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 
@@ -45,44 +47,71 @@ function App() {
     todos.splice(index, 1);
     setTodos([...todos]);
   };
+  const handleDeleteDone = () => {
+    const newTodos = todos.filter(todo => todo.done === false);
+    setTodos(newTodos);
+  };
+  const filteredTodos = todos.filter(
+    todo => (filter === 'all' && todo)
+      || (filter === 'done' && todo.done)
+      || (filter === 'notDone' && !todo.done),
+  );
+
   return (
     <div className="App">
-      <Typography component="h1" variant="h2">
-        React Todos
-      </Typography>
+      <div className="wrapper">
+        <Typography component="h1" variant="h2" className="title">
+          React Todos
+        </Typography>
 
-      <div>
-        <form onSubmit={handleSubmit}>
-          <TextField
-            id="text"
-            variant="outlined"
-            placeholder="タスクを追加する"
-            onChange={handleChange}
-          />
-        </form>
-      </div>
-      <div>
-        <IconButton onClick={() => setFilter('all')} disabled={filter === 'all'}>
-          全て
-        </IconButton>
-        <IconButton onClick={() => setFilter('done')} disabled={filter === 'done'}>
-          未完了
-        </IconButton>
-        <IconButton onClick={() => setFilter('notDone')} disabled={filter === 'notDone'}>
-          完了
-        </IconButton>
-      </div>
-      <div>
-        {todos
-          && todos
-            .filter(
-              todo => (filter === 'all' && todo)
-                || (filter === 'done' && todo.done)
-                || (filter === 'notDone' && !todo.done),
-            )
-            .map((todo, i) => (
+        <div className="form-block">
+          <form onSubmit={handleSubmit}>
+            <TextField
+              id="text"
+              variant="filled"
+              placeholder="タスクを入力"
+              onChange={handleChange}
+            />
+            <Button
+              className="add-buton"
+              variant="contained"
+              color="primary"
+              type="submit"
+              disabled={!text.length}
+            >
+              追加
+            </Button>
+            <Button
+              className="all-delete-buton"
+              variant="contained"
+              color="secondary"
+              disabled={!todos.find(todo => todo.done)}
+              onClick={() => {
+                handleDeleteDone();
+              }}
+            >
+              完了を一括削除
+            </Button>
+          </form>
+        </div>
+        <div className="filter-block">
+          <ButtonGroup>
+            <Button onClick={() => setFilter('all')} disabled={filter === 'all'}>
+              全て
+            </Button>
+            <Button onClick={() => setFilter('done')} disabled={filter === 'done'}>
+              未完了
+            </Button>
+            <Button onClick={() => setFilter('notDone')} disabled={filter === 'notDone'}>
+              完了
+            </Button>
+          </ButtonGroup>
+        </div>
+        <div className="list-block">
+          {filteredTodos.length ? (
+            filteredTodos.map((todo, i) => (
               <List>
-                <ListItem key={i.toString()} button onClick={handleDone(todo.id)}>
+                <ListItem dense button key={i.toString()} onClick={handleDone(todo.id)}>
                   <Checkbox
                     tabIndex={-1}
                     disableRipple
@@ -91,15 +120,19 @@ function App() {
                       handleDone(todo.id);
                     }}
                   />
-                  <ListItemText primary={todo.text} secondary={todo.done ? '完了' : '未完了'} />
+                  <ListItemText className={todo.done && 'done'} primary={todo.text} />
                   <ListItemSecondaryAction>
                     <IconButton onClick={handleDelete(todo.id)}>
-                      <DeleteIcon />
+                      <DeleteIcon color="error" />
                     </IconButton>
                   </ListItemSecondaryAction>
                 </ListItem>
               </List>
-            ))}
+            ))
+          ) : (
+            <p>タスクがありません</p>
+          )}
+        </div>
       </div>
     </div>
   );
