07c3db3c25cd2321483f2db21f938926abc1f876
suuungwoo <suuungwoo@gmail.com>
2019-07-07 23:09:18 +0900

    Updates

---
+++ precache-manifest.ecbc7bc71895bba9aab3e557fe92efb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b16674f7a98f34dd6b9e3411ce958fc",
+    "url": "/react-todos/index.html"
+  },
+  {
+    "revision": "e89671ea037e8cf25156",
+    "url": "/react-todos/static/css/main.10dd5345.chunk.css"
+  },
+  {
+    "revision": "52faeccabb62820b24f6",
+    "url": "/react-todos/static/js/2.4f6999ef.chunk.js"
+  },
+  {
+    "revision": "e89671ea037e8cf25156",
+    "url": "/react-todos/static/js/main.99699b73.chunk.js"
+  },
+  {
+    "revision": "ab9ea8a31f3c0d7bac35",
+    "url": "/react-todos/static/js/runtime~main.32ae1423.js"
+  }
+]);
